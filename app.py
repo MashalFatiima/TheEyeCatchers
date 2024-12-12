0c1eaa50ffd651713881b170a92cf0aa770fa174
Mashal Fatima <mashal.fatiima@gmail.com>
2024-12-12 18:36:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,8 @@ import base64
 from model.model import load_model, predict_and_visualize
 from utils.preprocess import preprocess_image
 
-# Set Streamlit page configuration
 st.set_page_config(page_title="TheEyeCatchers", layout="centered", page_icon="🖼️")
 
-# Custom CSS for styling
 st.markdown(
     """
     <style>
@@ -45,11 +43,9 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-# Load the model
 model, class_names = load_model()
 
-# Title section with logo
-logo_path = "C:\\Users\\Abdullah\\Desktop\\TheEyeCatchers\\Logo.png"  # Path to your logo file
+logo_path = "\Desktop\\TheEyeCatchers\\Logo.png"  
 if os.path.exists(logo_path):
     with open(logo_path, "rb") as f:
         logo_image = f.read()
@@ -67,35 +63,27 @@ if os.path.exists(logo_path):
 else:
     st.error("Logo file not found. Please ensure it's placed in the correct directory.")
 
-# File uploader
 uploaded_file = st.file_uploader("Upload an image for prediction", type=["jpg", "jpeg", "png"])
 
 if uploaded_file:
-    # Save uploaded image
     image_path = os.path.join("uploads", uploaded_file.name)
     with open(image_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
 
-    # Load and display the image
     image = Image.open(image_path).convert("RGB")
     st.image(image, caption="Uploaded Image", use_container_width=True)
 
-    # Preprocess the image
     input_tensor = preprocess_image(image)
-
-    # Make prediction and generate Grad-CAM
+    
     predicted_label, _, grad_cam_image = predict_and_visualize(model, input_tensor, image_path, class_names)
 
-    # Display prediction results
     st.subheader("Prediction:")
     prediction_text = "Diabetic Retinopathy Detected" if predicted_label == "DR" else "No Diabetic Retinopathy Detected"
     st.write(f"**{prediction_text}**")
 
-    # Display Grad-CAM
     st.subheader("Grad-CAM Visualization:")
     st.image(grad_cam_image, caption="Grad-CAM Overlay", use_container_width=True)
 
-# Footer
 st.markdown(
     """
     <div class="footer">
