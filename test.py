6edc463c64c7b6eee380f0b237d669411255f926
Mashal Fatima <mashal.fatiima@gmail.com>
2024-12-12 16:27:19 +0500

    Delete test.py

--- test.py
+++
@@ -1,4 +0,0 @@
-import torch
-
-model = torch.load(r"C:\Users\Abdullah\Desktop\TheEyeCatchers\model\trained_model.pth", map_location=torch.device('cpu'))
-print(type(model))  # Should output <class 'torchvision.models.resnet.ResNet'>
